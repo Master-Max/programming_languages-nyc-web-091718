c9eec27684048ca634fdc6e3fa2f6c57760789d1
Max Petersen <mglxp5@gmail.com>
2018-09-13 18:51:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  puts "===Start==="
+  hash = {}
+  #languages.collect {|x,y| puts"#{x.class} || #{y.class}"}
+  languages.collect do |x,y|
+    puts "#{x} || #{x.class}"
+    y.collect do |w,z|
+      puts "==#{w} || #{z.class}"
+      puts "====#{z} || #{z.class}"
+      z.collect do |a,b|
+        puts "======#{a} || #{a.class}"
+        puts "========#{b} || #{b.class}"
+      end
+    end
+  end
 end
