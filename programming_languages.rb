3a33655d88df3d24d9a208e1f8b65ad11f26460a
Max Petersen <mglxp5@gmail.com>
2018-09-13 19:29:57 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
 def reformat_languages(languages)
-  # your code here
+  puts "===Start==="
+  hash = {}
+  #languages.collect {|x,y| puts"#{x.class} || #{y.class}"}
+  languages.collect do |x,y|
+    puts "X: #{x} || #{x.class}"
+    y.collect do |w,z|
+      puts "W: ==#{w} || #{w.class}"
+      puts "Z: ====#{z} || #{z.class}"
+      #puts "====#{z.value} || #{z.value.class}"
+      z.collect do |a,b|
+        puts "A: ======#{a} || #{a.class}"
+        puts "B: ========#{b} || #{b.class}"
+        if hash.has_key?(w)
+          hash[w][:style].push(x)
+        else
+          hash[w] = {a => b , :style => [x]}
+        end
+      end
+    end
+  end
+  puts "=====+++++====="
+  puts languages
+  puts "=====-----====="
+  puts hash
+  puts "=====+++++====="
+  hash
 end
